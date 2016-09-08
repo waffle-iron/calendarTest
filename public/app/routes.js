20dc1eefdfece3252cf30ae5de8fb49f2fbf6e84
Vincent <vincentnewkirk@gmail.com>
2016-09-08 11:58:33 -1000

    complete routes for all links. app is now a SPA

--- public/app/routes.js
+++ public/app/routes.js
@@ -2,5 +2,14 @@ angular.module('CalendarTest')
   .config(function($routeProvider){
     $routeProvider.when('/signup', {
       templateUrl: '/signup.html'
-    });
+    })
+    $routeProvider.when('/login', {
+      templateUrl: '/login.html'
+    })
+    $routeProvider.when('/testPage', {
+      templateUrl: '/testPage.html'
+    })
+    $routeProvider.when('/visitform', {
+      templateUrl: '/visitform.html'
+    })
   });
